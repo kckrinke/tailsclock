22af67971bb6a7a0648fc017aeb771953b7616f0
Kevin C. Krinke <kevin@krinke.ca>
2013-10-17 20:00:10 -0400

    Refactored prefs, added new config options and implemented a much more reliable method of adjusting the date/time format

--- TailsClockApplet.py
+++ TailsClockApplet.py
@@ -31,6 +31,8 @@ except: # Can't use ImportError, as gi.repository isn't quite that nice...
     import gobject as GObject
     import gtk.gdk as Gdk
 
+# Consolidate default values
+DEFAULT_CFG_DATA = {'show_sec':False,'show_12hr':False,'show_tz':False,'tz':'UTC'}
 
 class TailsClockPrefsDialog:
     """
@@ -41,6 +43,9 @@ class TailsClockPrefsDialog:
     dialog = None
     tz_tview = None
     tz_store = None
+    show_12hr = None
+    show_tz = None
+    show_sec = None
 
     def __init__(self,applet):
         #: store the applet's reference for later
@@ -51,10 +56,33 @@ class TailsClockPrefsDialog:
                                  )
         #: construct the core dialog
         vbox = self.dialog.get_content_area()
+        #: setup a notebook
+        nbook = Gtk.Notebook()
+        nbook.set_show_tabs(True)
+        nbook.set_show_border(True)
+        vbox.pack_start(nbook,True,True,8)
+        #: General Settings
+        tbl = Gtk.VBox()
+        self.show_12hr = Gtk.CheckButton(_("12 Hour Clock?"))
+        self.show_12hr.set_active(self.applet.show_12hr)
+        self.show_12hr.connect('toggled',self.toggled_12hr)
+        tbl.pack_start(self.show_12hr,True,True,8)
+        self.show_sec = Gtk.CheckButton(_("Display Seconds?"))
+        self.show_sec.set_active(self.applet.show_sec)
+        self.show_sec.connect('toggled',self.toggled_sec)
+        tbl.pack_start(self.show_sec,True,True,8)
+        self.show_tz = Gtk.CheckButton(_("Display Timezone?"))
+        self.show_tz.set_active(self.applet.show_tz)
+        self.show_tz.connect('toggled',self.toggled_tz)
+        tbl.pack_start(self.show_tz,True,True,8)
+        nbook.append_page(tbl,Gtk.Label(_("General")))
         #: Time Zone Configuration
+        tz_vbox = Gtk.VBox()
+        nbook.append_page(tz_vbox,Gtk.Label(_("Timezone")))
         tz_label = Gtk.Label(_("Please select a timezone from the following list:"))
         tz_label.set_line_wrap(True)
-        vbox.pack_start(tz_label,False,False,1)
+        tz_label.set_width_chars(42)
+        tz_vbox.pack_start(tz_label,False,False,8)
         self.tz_store = Gtk.ListStore(str)
         self.tz_tview = Gtk.TreeView(self.tz_store)
         self.tz_tview.set_headers_visible(False)
@@ -64,7 +92,8 @@ class TailsClockPrefsDialog:
         tz_scroll = Gtk.ScrolledWindow(hadjustment=None,vadjustment=None)
         tz_scroll.set_policy(Gtk.PolicyType.AUTOMATIC,Gtk.PolicyType.AUTOMATIC)
         tz_scroll.add(self.tz_tview)
-        vbox.pack_start(tz_scroll,True,True,1)
+        tz_frame = Gtk.Frame()
+        tz_frame.add(tz_scroll)
         #: Add timezones to the list and pre-select the current timezone
         select = self.tz_tview.get_selection()
         for tz in pytz.common_timezones:
@@ -77,11 +106,31 @@ class TailsClockPrefsDialog:
                     pass
                 pass
             pass
+        tz_vbox.pack_start(tz_frame,True,True,8)
         select.connect("changed",self.on_timezone_selection_changed)
         select.set_mode(Gtk.SelectionMode.SINGLE)
         #: try to constrain the dialog's size
-        self.dialog.set_size_request(250,250)
+        nbook.show_all()
+        self.dialog.set_size_request(280,280)
         self.dialog.set_resizable(False)
+        pass
+
+    def update_general(self):
+        data = DEFAULT_CFG_DATA.copy()
+        data['show_12hr'] = self.show_12hr.get_active()
+        data['show_sec'] = self.show_sec.get_active()
+        data['show_tz'] = self.show_tz.get_active()
+        self.applet.update_cfg(data)
+        pass
+
+    def toggled_12hr(self,widget):
+        self.update_general()
+        pass
+    def toggled_sec(self,widget):
+        self.update_general()
+        pass
+    def toggled_tz(self,widget):
+        self.update_general()
         pass
 
     def on_timezone_selection_changed(self,selection):
@@ -119,9 +168,16 @@ class TailsClock:
     panel_iid = None
     panel_data = None
 
+    glib_timer = None
+
     tz_info = None
     tz_name = None
     cfg_path = None
+    cfg_tz_path = None
+
+    show_tz = DEFAULT_CFG_DATA['show_tz']
+    show_sec = DEFAULT_CFG_DATA['show_sec']
+    show_12hr = DEFAULT_CFG_DATA['show_12hr']
 
     def __init__(self,applet,iid,data):
         """
@@ -133,7 +189,8 @@ class TailsClock:
         else:
             # debug; load translations from "here"
             gettext.install('tailsclockapplet', './locale', unicode=1)
-        self.cfg_path = os.environ['HOME']+"/.config/tails/timezone"
+        self.cfg_path = os.environ['HOME']+"/.config/tails/tailsclock"
+        self.cfg_tz_path = os.environ['HOME']+"/.config/tails/timezone"
         self.panel_applet = applet
         self.panel_iid = iid
         self.panel_data = data
@@ -157,20 +214,25 @@ class TailsClock:
         Read the contents of the configuration file and update the clock
         accordingly. Tries to handle failures gracefully.
         """
-        if os.path.exists(self.cfg_path):
-            fh = open(self.cfg_path,'r')
+        if os.path.exists(self.cfg_tz_path):
+            contents = self._read_file(self.cfg_tz_path)
             try:
-                contents = fh.read()
-                contents = re.sub("\r??\n","",contents)
                 self.tz_info = pytz.timezone(contents.strip())
                 self.tz_name = contents
             except Exception, e:
                 sys.stderr.write("TailsClock Invalid Timezone: "+str(e)+"\n")
                 self.tz_info = None
-                self.tz_name = contents
-            fh.close()
         if self.tz_info is None:
+            self.tz_name = "UTC"
             self.tz_info = pytz.utc
+        if os.path.exists(self.cfg_path):
+            data = self._read_yaml(self.cfg_path)
+            if data.has_key('show_tz'):
+                self.show_tz = bool(data['show_tz'])
+            if data.has_key('show_sec'):
+                self.show_sec = bool(data['show_sec'])
+            if data.has_key('show_12hr'):
+                self.show_12hr = bool(data['show_12hr'])
         return
 
     def _write_file(self,path,contents):
@@ -229,14 +291,12 @@ class TailsClock:
         Overwrites the timezone configuration file with the given data.
         """
         if data.has_key('tz'):
-            fh = open(self.cfg_path,'w')
-            try:
-                fh.write(data['tz'])
-                pass
-            except Exception, e:
-                os.remove(self.cfg_path)
-            fh.close()
+            self._write_file(self.cfg_tz_path,data['tz'])
+            del data['tz']
+        if len(data) > 0:
+            self._write_yaml(self.cfg_path,data)
         self.refresh_cfg()
+        self.update_time()
         return
 
     def launch(self):
@@ -245,7 +305,6 @@ class TailsClock:
         and so on. Basically; launch the applet.
         """
         # actually populate UI
-        self.refresh_cfg()
         self.main_label = Gtk.Label("...")
         self.main_label.set_name("TailsClockAppletLabel")
         self.main_evbox = Gtk.EventBox()
@@ -269,22 +328,53 @@ class TailsClock:
         # add/show all
         self.panel_applet.add(self.main_evbox)
         self.panel_applet.show_all()
+        self.refresh_cfg()
         self.update_time()
-        GObject.timeout_add(1000,self.update_time)
         return self
+
+    def update_glib_timer(self):
+        if self.glib_timer != None:
+            GObject.source_remove(self.glib_timer)
+            self.glib_timer = None
+        if self.show_sec:
+            self.glib_timer = GObject.timeout_add(1000,self.update_time)
+        else:
+            now = datetime.now()
+            min = datetime(now.year,now.month,now.day,now.hour,now.minute+1)
+            delta = (min-now).seconds * 1000 + 999
+            self.glib_timer = GObject.timeout_add(delta,self.update_time)
+        return False
 
     def update_time(self):
         """
         Fired once-per second to update the time.
         """
+        # get the date/time and apply tz_info to it
         utc_dt = datetime.utcnow()
         utc_dt = utc_dt.replace(tzinfo=pytz.utc)
         dt = utc_dt.astimezone(self.tz_info)
+        # format the date/time stamp
         dt_format = locale.nl_langinfo(locale.D_T_FMT)
+        if '%r' in dt_format:
+            if self.show_sec:
+                if self.show_12hr:
+                    dt_format = re.sub("%r","%I:%M:%S %p",dt_format)
+                else:
+                    dt_format = re.sub("%r","%H:%M:%S",dt_format)
+            else:
+                if self.show_12hr:
+                    dt_format = re.sub("%r","%I:%M %p",dt_format)
+                else:
+                    dt_format = re.sub("%r","%H:%M",dt_format)
+                pass
+        else:
+            sys.stderr.write("TailsClock: there's no %%r?!")
+        if '%Z' in dt_format and not self.show_tz:
+            dt_format = re.sub("\s*%Z","",dt_format)
         stamp = dt.strftime(dt_format)
-        stamp = re.sub("(\d+)\:(\d+)\:(\d+)","\\1:\\2",stamp) #: remove seconds
+        # actually update the label
         self.main_label.set_text(stamp)
-        return True
+        return self.update_glib_timer()
 
     def display_prefs(self,*argv):
         """
