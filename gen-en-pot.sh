183df158109dd3717808a2f2549276def53f6b67
Kevin C. Krinke <kevin@krinke.ca>
2013-11-03 21:50:07 -0500

    fixed pot/en.po generation script to handle multiline strings better.

--- gen-en-pot.sh
+++ gen-en-pot.sh
@@ -7,5 +7,5 @@ xgettext --add-comments=Translators --language=Python --omit-header -o ${MESSAGE
 echo "done"
 echo -n "Creating EN PO... "
 cat ${MESSAGES_POT} | \
-    perl -e '$r="";while(my $l=<>){$r.=$l;};$r=~s!msgid "([^"]*)"(\s*\r??\n\s*msgstr) ""!msgid "$1"$2 "$1"!gms;$r=~s!CHARSET!UTF-8!g;print $r;' > ${MESSAGES_PO}
+    perl -e '$r="";while(my $l=<>){$r.=$l;};$r=~s!msgid (.+?)(\s*\r??\n\s*msgstr) ""!msgid $1$2 $1!gms;$r=~s!CHARSET!UTF-8!g;print $r;' > ${MESSAGES_PO}
 echo "done"
